8618e1e6a3724276870ded8a8a63574230df1dab
Wundero <Wundero@users.noreply.github.com>
2024-11-29 22:40:06 -0800

    Add process env cf creds

--- apps/deployment/drizzle.config.ts
+++ apps/deployment/drizzle.config.ts
@@ -4,4 +4,12 @@ export default defineConfig({
   dialect: "sqlite",
   schema: "./src/db/schema.ts",
   driver: "d1-http",
+  dbCredentials: {
+    // @ts-expect-error - Process is defined here because this is run via CLI
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    // @ts-expect-error - Process is defined here because this is run via CLI
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    // @ts-expect-error - Process is defined here because this is run via CLI
+    token: process.env.CLOUDFLARE_D1_TOKEN!,
+  },
 });
