2b767627e8d89d168c3c1b6361a479229e45ceb3
Wundero <Wundero@users.noreply.github.com>
2024-12-01 04:12:07 -0800

    Hooks to listen to specific events

--- packages/react/src/hook.tsx
+++ packages/react/src/hook.tsx
@@ -2,7 +2,7 @@
 
 import React, { useMemo } from "react";
 
-import type { Channel, PresenceChannel, Sinker } from "@sinkr/core";
+import type { Channel, EventMap, PresenceChannel, Sinker } from "@sinkr/core";
 import { sink } from "@sinkr/core";
 
 interface SinkrContext {
@@ -49,6 +49,29 @@ export function useSinkr() {
 }
 
 /**
+ * Run a callback whenever the specified event is fired.
+ * @param event The event to listen to.
+ * @param callback A callback to run when the event is fired.
+ */
+export function useSinkrEvent<
+  TEvent extends keyof EventMap,
+  TData extends EventMap[TEvent] = EventMap[TEvent],
+>(event: TEvent, callback: (data: TData) => void) {
+  const sinkr = useSinkr();
+  React.useEffect(() => {
+    if (!sinkr) {
+      return;
+    }
+    const unsubFn = sinkr.on(event, (data) => {
+      callback(data.message as TData);
+    });
+    return () => {
+      unsubFn();
+    };
+  }, [sinkr, event, callback]);
+}
+
+/**
  * Get an event listener for a specific channel.
  * @param channel The channel name.
  */
@@ -61,4 +84,28 @@ export function useSinkrChannel(
 ): Channel | PresenceChannel | null {
   const sinkr = useSinkr();
   return sinkr?.channel(channel) ?? null;
+}
+
+/**
+ * Run a callback whenever the specified event is fired on the specified channel.
+ * @param channel The channel name.
+ * @param event The event to listen to.
+ * @param callback A callback to run when the event is fired.
+ */
+export function useSinkrChannelEvent<
+  TEvent extends keyof EventMap,
+  TData extends EventMap[TEvent] = EventMap[TEvent],
+>(channel: string, event: TEvent, callback: (data: TData) => void) {
+  const ch = useSinkrChannel(channel);
+  React.useEffect(() => {
+    if (!ch) {
+      return;
+    }
+    const unsubFn = ch.on(event, (data) => {
+      callback(data as TData);
+    });
+    return () => {
+      unsubFn();
+    };
+  }, [ch, event, callback]);
 }
