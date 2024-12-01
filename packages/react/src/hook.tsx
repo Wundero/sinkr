39d09c3dd8c417da59bcb85afbd7888b53c85c01
Wundero <Wundero@users.noreply.github.com>
2024-12-01 04:06:05 -0800

    Pretty much done with main sdk

---
+++ packages/react/src/hook.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useMemo } from "react";
+
+import type { Channel, PresenceChannel, Sinker } from "@sinkr/core";
+import { sink } from "@sinkr/core";
+
+interface SinkrContext {
+  sink: Sinker;
+}
+
+const SinkrContext = React.createContext<SinkrContext | null>(null);
+
+export interface SinkrProviderProps {
+  url: string;
+  appId?: string;
+  children: React.ReactNode;
+}
+
+/**
+ * Create a SinkrProvider to provide the sink instance to the children.
+ *
+ * All children of this provider can call `useSinkr` or `useSinkrChannel` to get an event listener for the sink.
+ */
+export function SinkrProvider({ url, appId, children }: SinkrProviderProps) {
+  const memoizedSink = useMemo(() => {
+    return sink({
+      url,
+      appId,
+    });
+  }, [url, appId]);
+
+  return (
+    <SinkrContext.Provider value={{ sink: memoizedSink }}>
+      {children}
+    </SinkrContext.Provider>
+  );
+}
+
+/**
+ * Get an event listener for all events.
+ */
+export function useSinkr() {
+  const context = React.useContext(SinkrContext);
+  if (!context) {
+    return null;
+  }
+  return context.sink;
+}
+
+/**
+ * Get an event listener for a specific channel.
+ * @param channel The channel name.
+ */
+export function useSinkrChannel(channel: string): Channel | null;
+export function useSinkrChannel(
+  channel: `presence-${string}`,
+): PresenceChannel | null;
+export function useSinkrChannel(
+  channel: string,
+): Channel | PresenceChannel | null {
+  const sinkr = useSinkr();
+  return sinkr?.channel(channel) ?? null;
+}
