79a176d6c12efc36bab721da1fb5a956c5ef6f3e
Wundero <Wundero@users.noreply.github.com>
2025-01-09 22:04:54 -0800

    Encryption for messages

--- packages/validators/src/schemas.ts
+++ packages/validators/src/schemas.ts
@@ -19,24 +19,51 @@ export const UnsubscribeRouteSchema = z.object({
   channel: z.string(),
 });
 
+export const MessageTypeSchema = z.discriminatedUnion("type", [
+  // Unencrypted message
+  z.object({
+    type: z.literal("plain"),
+    message: z.unknown(),
+  }),
+  // Unencrypted message part
+  z.object({
+    type: z.literal("chunk"),
+    index: z.number(),
+    message: z.unknown(),
+  }),
+  // Encrypted message
+  z.object({
+    type: z.literal("encrypted"),
+    ciphertext: z.string(),
+    keyId: z.string(),
+  }),
+  // Encrypted message part
+  z.object({
+    type: z.literal("encrypted-chunk"),
+    ciphertext: z.string(),
+    keyId: z.string(),
+    index: z.number(),
+  }),
+]);
+
 export const ChannelMessageSchema = z.object({
   route: z.literal("channel"),
   channel: z.string(),
   event: z.string(),
-  message: z.unknown(),
+  message: MessageTypeSchema,
 });
 
 export const DirectMessageSchema = z.object({
   route: z.literal("direct"),
   recipientId: z.string(),
   event: z.string(),
-  message: z.unknown(),
+  message: MessageTypeSchema,
 });
 
 export const BroadcastMessageSchema = z.object({
   route: z.literal("broadcast"),
   event: z.string(),
-  message: z.unknown(),
+  message: MessageTypeSchema,
 });
 
 export const ServerEndpointSchema = z.discriminatedUnion("route", [
@@ -107,7 +134,7 @@ export const ClientReceiveMessageSchema = z.object({
       channel: z.string(),
     }),
   ]),
-  message: z.unknown(),
+  message: MessageTypeSchema,
 });
 
 export const ClientReceiveSchema = z.discriminatedUnion("source", [
